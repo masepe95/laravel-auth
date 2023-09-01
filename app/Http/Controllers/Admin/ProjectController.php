375c68cbfebf0e71e54eba1bb7fdbe33c4dece11
masepe95 <marcosepe10@gmail.com>
2023-09-01 09:04:44 +0200

    orderBy query - removed register option

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::orderBy('updated_at', 'DESC')->get();
         return view('admin.projects.index', compact('projects'));
     }
 

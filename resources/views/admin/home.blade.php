b8cd08ceed27fb7ef2d68bb6d23826f50bb41999
masepe95 <marcosepe10@gmail.com>
2023-09-04 08:59:38 +0200

    changed login page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,7 +7,7 @@
     </h2>
     <div class="row justify-content-center">
         <div class="col">
-            <div class="card">
+            <div class="card mb-5">
                 <div class="card-header">{{ __('User Dashboard') }}</div>
 
                 <div class="card-body">
